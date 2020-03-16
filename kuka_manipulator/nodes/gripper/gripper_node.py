b19d57766034b5bd3679f2f382994c32920b1eb4
charlotteheggem <charlotteheggem@hotmail.com>
2020-03-16 11:27:24 +0100

    Add methods for opening, activiating, closing and reading response from gripper

--- kuka_manipulator/nodes/gripper/gripper_node.py
+++ kuka_manipulator/nodes/gripper/gripper_node.py
@@ -4,6 +4,8 @@ import _thread as thread
 import time
 import os
 import sys
+import serial
+import binascii
 import math
 import rclpy
 from rclpy.node import Node
@@ -26,7 +28,9 @@ class GripperNode(Node):
     def __init__(self):
         super().__init__('gripper_node')
         self.name='gripper_node'
-
+        # TODO: change port to NUC
+        self.ser = serial.Serial(port="/dev/ttyUSB1", baudrate=115200, timeout=1, parity=serial.PARITY_NONE,
+                            stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 
         # Make Publishers for relevant data
         #self.pub_laserscan1 = self.create_publisher(LaserScan, 'scan', qos_profile_sensor_data)
@@ -59,6 +63,44 @@ class GripperNode(Node):
         timestamp.nanosec = int((t - timestamp.sec) * 10 ** 9)
         return timestamp
 
+    def activate(self):
+        #Activation Request
+        self.ser.write(b"\x09\x10\x03\xE8\x00\x03\x06\x00\x00\x00\x00\x00\x00\x73\x30")
+        data = binascii.hexlify(self.ser.readline()).decode()
+
+        #Status Request until activation complere
+        Activated=False
+        CompleteString = "09030200005985"
+        while(not Activated):
+            self.ser.write(b"\x09\x03\x07\xD0\x00\x01\x85\xCF")
+            data = binascii.hexlify(self.ser.readline()).decode()
+            if(data==CompleteString):
+                Activated=True
+
+
+    def getClosedResponse(self):
+        data_raw = self.ser.readline()
+        data = binascii.hexlify(data_raw).decode()
+        # gripperstatus - null - fault - pos request echo - position - current/force - <null>
+
+        #closing Not complete: 09 03 06 39 00 00 FF 0E 0A F7 8B
+        #closing Complete 09 03 06 B9 00 00 FF BD 00 1D 7C
+    def getOpenResponse(self):
+
+        #Request 09 03 07 D0 00 03 04 0 E
+        # opening incomplete 09 03 06 39 00 00 00 BB 10 30 E0
+        # opening complete : 09 03 06 F9 00 00 00 0D 00 56 4C
+
+
+    def close(self):
+        # Close the gripper
+        # actionrequest - null - null - position - speed - force - <null>
+        # to siste FF skal halveres
+        self.ser.write(b"\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\xFF\xFF\xFF\x42\x29")
+
+    def open(self):
+        # Open the gripper full speed and force
+        self.ser.writeser.write(b"\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\x00\xFF\xFF\x72\x19")
 
 def main(args=None):
     rclpy.init(args=args)
