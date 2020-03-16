0ae139cb9ffc683b600eb0f909571325cc31e93f
charlotteheggem <charlotteheggem@hotmail.com>
2020-03-16 17:05:17 +0100

    implement methods to open/close gripper and read status

--- kuka_manipulator/nodes/gripper/gripper_node.py
+++ kuka_manipulator/nodes/gripper/gripper_node.py
@@ -9,7 +9,6 @@ import binascii
 import math
 import rclpy
 from rclpy.node import Node
-import socket
 from std_msgs.msg import String
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3, TransformStamped
 from nav_msgs.msg import Odometry
@@ -18,10 +17,24 @@ from builtin_interfaces.msg import Time
 from tf2_ros.transform_broadcaster import TransformBroadcaster
 from tf2_ros import StaticTransformBroadcaster
 from rclpy.qos import qos_profile_sensor_data
-
-
+from enum import Enum
 def cl_red(msge): return '\033[31m' + msge + '\033[0m'
 
+
+class Msg(Enum):
+    Activation = b"\x09\x10\x03\xE8\x00\x03\x06\x00\x00\x00\x00\x00\x00\x73\x30"
+    ActivationRequest = b"\x09\x03\x07\xD0\x00\x01\x85\xCF"
+    ActivationComplete = "09030200005985"
+    
+    StatusResponse= b"\x09\x03\x07\xD0\x00\x03\x04\x0E"
+    CloseRequest = b"\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\xFF\x7F\x7F\x22\x49"
+    OpenRequest = b"\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\x00\xFF\xFF\x72\x19"
+
+    NOTMOVING = -1
+    MOVING = 0
+    OBJECT_OPENING = 1
+    OBJECT_CLOSING = 2
+    REQUESTEDPOSITION = 3
 
 
 class GripperNode(Node):
@@ -37,16 +50,16 @@ class GripperNode(Node):
         #self.send_static_transform()
 
 
-        while not self.soc.isconnected:
-            pass
         self.get_logger().info('Node is ready')
 
         thread.start_new_thread(self.run, ())
 
     def run(self):
-        while rclpy.ok() and self.soc.isconnected:
+        while rclpy.ok():
             if True:
-                self.scan_callback(self.pub_laserscan1, self.soc.laserScanB1.pop(0))
+                #self.scan_callback(self.pub_laserscan1, self.soc.laserScanB1.pop(0))
+                print()
+
 
 
     def scan_callback(self, publisher, values):
@@ -64,43 +77,87 @@ class GripperNode(Node):
         return timestamp
 
     def activate(self):
-        #Activation Request
-        self.ser.write(b"\x09\x10\x03\xE8\x00\x03\x06\x00\x00\x00\x00\x00\x00\x73\x30")
-        data = binascii.hexlify(self.ser.readline()).decode()
-
-        #Status Request until activation complere
-        Activated=False
-        CompleteString = "09030200005985"
-        while(not Activated):
-            self.ser.write(b"\x09\x03\x07\xD0\x00\x01\x85\xCF")
-            data = binascii.hexlify(self.ser.readline()).decode()
-            if(data==CompleteString):
-                Activated=True
-
-
-    def getClosedResponse(self):
+        # Activation Request
+        self.ser.write(Msg.Activation.value)
         data_raw = self.ser.readline()
-        data = binascii.hexlify(data_raw).decode()
-        # gripperstatus - null - fault - pos request echo - position - current/force - <null>
-
-        #closing Not complete: 09 03 06 39 00 00 FF 0E 0A F7 8B
-        #closing Complete 09 03 06 B9 00 00 FF BD 00 1D 7C
-    def getOpenResponse(self):
-
-        #Request 09 03 07 D0 00 03 04 0 E
-        # opening incomplete 09 03 06 39 00 00 00 BB 10 30 E0
-        # opening complete : 09 03 06 F9 00 00 00 0D 00 56 4C
-
+        Activated = False
+        while (not Activated):
+            self.ser.write(Msg.ActivationRequest.value)
+            data_raw = self.ser.readline()
+            data_string = binascii.hexlify(data_raw).decode()
+            if (data_string == Msg.ActivationComplete.value):
+                Activated = True
+                self.close()
+                self.open()
 
     def close(self):
         # Close the gripper
-        # actionrequest - null - null - position - speed - force - <null>
-        # to siste FF skal halveres
-        self.ser.write(b"\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\xFF\xFF\xFF\x42\x29")
+        self.ser.write(Msg.CloseRequest.value)
+        self.ser.readline()
 
     def open(self):
         # Open the gripper full speed and force
-        self.ser.writeser.write(b"\x09\x10\x03\xE8\x00\x03\x06\x09\x00\x00\x00\xFF\xFF\x72\x19")
+        self.ser.write(Msg.OpenRequest.value)
+        self.ser.readline()
+
+    def getClosedResponse(self):
+        result = False
+        while (not result):
+            self.ser.write(Msg.StatusResponse.value)
+            data_raw = self.ser.readline()
+            print(data_raw)
+            gObj = self.messageTogObj(str(data_raw))
+            # Not Moving
+            if(gObj==Msg.NOTMOVING.value):
+                result=False
+                print("Not moving")
+            # Moving
+            if(gObj==Msg.MOVING.value):
+                result=False
+                print("Closing")
+            # Closed, no object:
+            if(gObj==Msg.REQUESTEDPOSITION.value):
+                result=False
+                print("No object found while closing")
+            # Closed, object:
+            if(gObj==Msg.OBJECT_CLOSING.value):
+                result=True
+                print("Object found while closing")
+        return result
+
+    def getOpenResponse(self):
+        result=False
+        while(not result):
+            self.ser.write(Msg.StatusResponse.value)
+            gObj = self.messageTogObj(str(self.ser.readline()))
+            if(gObj==Msg.NOTMOVING.value):
+                result=False
+                print("Not moving")
+            #Moving
+            if (gObj == Msg.MOVING.value):
+                result = False
+                print("Opening")
+            # Collision
+            if (gObj == Msg.OBJECT_OPENING.value):
+                result = False
+                print("Collision")
+            # Open
+            if (gObj == Msg.REQUESTEDPOSITION.value):
+                result = True
+                print("Open: Requested Position")
+        return result
+
+
+    def messageTogObj(self, data_string):
+        # moving only meaningful if gGTO = 1
+        hexa = data_string.split("\\")[4].split("x")[1]
+        print(hexa)
+        binary = bin(int(hexa, 16))[2:].zfill(8)
+        gGTO = binary[4]
+        gObj = 2*int(binary[0]) + 1*int(binary[1])
+        if (gObj==0 and gGTO!=1):
+            gObj=-1
+        return gObj
 
 def main(args=None):
     rclpy.init(args=args)
