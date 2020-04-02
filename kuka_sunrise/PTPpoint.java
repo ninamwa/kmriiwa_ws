2e694a917ab7a6ad5d7e354a7ae7fbdbeb7dab49
charlotteheggem <charlotteheggem@hotmail.com>
2020-04-02 08:55:03 +0200

    Add spline motion implementation and PTP class to LBR_commander

---
+++ kuka_sunrise/PTPpoint.java
@@ -0,0 +1,31 @@
+package API_ROS2_Sunrise;
+
+
+import com.kuka.roboticsAPI.deviceModel.JointPosition;
+import com.kuka.roboticsAPI.motionModel.PTP;
+import com.kuka.roboticsAPI.motionModel.SplineMotionJP;
+
+
+public class PTPpoint{
+	PTP ptppoint;
+	String pointtype;
+	
+	public PTPpoint(String type, JointPosition jp, double[] vel, double[] acc){
+		ptppoint = new PTP(jp);
+		ptppoint.setJointVelocityRel(vel);
+		ptppoint.setJointAccelerationRel(acc);
+		pointtype = type;
+	}
+	
+	
+	public PTP getPTP(){
+		return ptppoint;
+	}
+	
+	public String getPointType(){
+		return pointtype;
+	}
+	
+
+	
+	}
