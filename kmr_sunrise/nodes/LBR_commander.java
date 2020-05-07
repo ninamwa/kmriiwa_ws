dc94332bbf8dae65fff433db055d27402b9496da
Charlotte Heggem <44672581+charlotteheggem@users.noreply.github.com>
2020-05-07 14:31:08 +0200

    Upload files from KMR2

--- kmr_sunrise/nodes/LBR_commander.java
+++ kmr_sunrise/nodes/LBR_commander.java
@@ -18,36 +18,37 @@ package API_ROS2_Sunrise;
 // Implemented classes
 
 // Robotics API
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
-import API_ROS2_Sunrise.PTPpoint;
 import API_ROS2_Sunrise.ISocket;
 import API_ROS2_Sunrise.TCPSocket;
 import API_ROS2_Sunrise.UDPSocket;
+import API_ROS2_Sunrise.PTPpoint;
+
 
 // RoboticsAPI
+import com.kuka.condition.ICondition;
 import com.kuka.roboticsAPI.deviceModel.JointPosition;
 import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.executionModel.ExecutionState;
 import com.kuka.roboticsAPI.executionModel.ICommandContainer;
+import com.kuka.roboticsAPI.executionModel.IExecutionContainer;
 import com.kuka.roboticsAPI.geometricModel.AbstractFrame;
-import com.kuka.roboticsAPI.geometricModel.LoadData;
-import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.ObjectFrame;
+import com.kuka.roboticsAPI.motionModel.IMotion;
 import com.kuka.roboticsAPI.motionModel.IMotionContainer;
+import com.kuka.roboticsAPI.motionModel.IMotionContainerListener;
+import com.kuka.roboticsAPI.motionModel.PTP;
 import com.kuka.roboticsAPI.motionModel.SplineJP;
 import com.kuka.roboticsAPI.motionModel.SplineMotionJP;
+import com.kuka.roboticsAPI.motionModel.SplineRuntimeData;
 
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptp;
 
-public class LBR_commander extends Thread{
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class LBR_commander extends Node{
 	
-	// Runtime Variables
-	public volatile boolean shutdown = false;
-	public volatile boolean closed = false;
-	public volatile boolean EmergencyStop = false;
-	boolean isKMPConnected;
 
 	// Robot Specific
 	LBR lbr;
@@ -58,169 +59,149 @@ public class LBR_commander extends Thread{
 	private final double defaultVelocity = 0.2;
 	AbstractFrame drivePos;
 	ICommandContainer LBR_currentMotion;
-	public volatile boolean LBR_is_Moving = false;
+	private int jointCount;
+	private static final double zero = Math.pow(10,-40);
 
-	// Socket
-	ISocket socket;
-	String ConnectionType;
-	String CommandStr;
-	int port;
 	
-	Tool tool;
-	
+	// Spline Motion
+	double[] poses;
+	double[] velocities;
+	double[] accelerations;
 	private List<SplineMotionJP<?>> splineSegments = new ArrayList<SplineMotionJP<?>>();
-	PathThread followPathThread;
 
 
-	public LBR_commander(int port, LBR robot, String ConnectionType, AbstractFrame drivepos) {
-		this.port = port;
+	public LBR_commander(int port,LBR robot, String ConnectionType, AbstractFrame drivepos) {
+		super(port, ConnectionType, "LBR commander");
+		
 		this.lbr = robot;
-		this.ConnectionType = ConnectionType;
 		this.drivePos = drivepos;
-		lbr.setHomePosition(drivePos);
 		
-//		LoadData load = new LoadData(0.9, null, null);
-//		tool = new Tool("Gripper", load);
-//		tool.attachTo(lbr.getFlange());
-		
-		createSocket();
+		jointCount=lbr.getJointCount();
+		poses = new double[jointCount];
+		velocities = new double[jointCount];
+		accelerations = new double[jointCount];
 		
 		if (!(isSocketConnected())) {
 			System.out.println("Starting thread to connect LBR command node....");
 			Thread monitorLBRCommandConnections = new MonitorLBRCommandConnectionsThread();
 			monitorLBRCommandConnections.start();
+			}else {
+				setisLBRConnected(true);
 			}
+
 	}
 	
-
+	@Override
 	public void run() {
 		Thread emergencystopthread = new MonitorEmergencyStopThread();
 		emergencystopthread.start();
 		
-		followPathThread = new PathThread();
 		CommandedjointPos = lbr.getCurrentJointPosition();
 		
-		while(isSocketConnected() && (!(closed)))
+		while(isNodeRunning())
 		{   
 			String Commandstr = socket.receive_message(); 
 	    	String []splt = Commandstr.split(" ");
 	    	
 	    	if ((splt[0]).equals("shutdown")){
 	    		System.out.println("LBR RECEIVED SHUTDOWN");
-				this.shutdown = true;
-				break;
+	    		setShutdown(true);
+	    		break;
 				}
-	    	
-			if ((splt[0]).equals("setLBRmotion")&& (!(getEmergencyStop()))){
+			if ((splt[0]).equals("setLBRmotion")){
 				JointLBRMotion(Commandstr);
 				}
-			if ((splt[0]).equals("pathPointLBR")&& (!(getEmergencyStop()))){
+			if ((splt[0]).equals("pathPointLBR")){
 				addPointToSegment(Commandstr);
 				}
 		}
     }
-	
-	public void runmainthread(){
-		this.run();
-	}
-	
-	// TODO: tenke over hva som skjer ved en emergencystop
-	private class PathThread extends Thread {
-		public void run(){
-			if( !(getEmergencyStop()) && !splineSegments.isEmpty() && (isSocketConnected()) && (!(closed)) ){
-					SplineJP spline = new SplineJP(splineSegments.toArray(new SplineMotionJP<?>[splineSegments.size()]));
-					LBR_is_Moving = true;
-					currentmotion = lbr.move(spline);
-					if(currentmotion.isFinished()){
-						LBR_is_Moving = false;
-						
-//						TODO: blir dette krøll? hvis en ny segment sendes inn mens arm beveger seg?
-						splineSegments.clear();
-						// TODO: DO SOMETHING WITH THE GRIPPER :D
-					}
-					}else{
-						LBR_is_Moving = false;
-					}
-				}
-			
-		}
 
 	private void addPointToSegment(String commandstr){
-		int ArrayLength = lbr.getJointCount();
 		String []lineSplt = commandstr.split(">");
 		String pointType = lineSplt[1];
 		
-
-		double[] poses = new double[ArrayLength];
-		double[] velocities = new double[ArrayLength];;
-		double[] accelerations = new double[ArrayLength];;
-		
-		
-		// Read message
-		for(int i = 0; i < ArrayLength ; ++i){
-			poses[i] = Double.parseDouble(lineSplt[2].split(" ")[i]);
-			double vel = Math.abs(Double.parseDouble(lineSplt[3].split(" ")[i]));
-			if(vel>1){
-				vel = 1;
-			}else if(vel<-1){
-				vel = -1;
-			}
-			velocities[i] = vel;
-			double accel = Math.abs(Double.parseDouble(lineSplt[4].split(" ")[i]));
-			if(accel>1){
-				accel = 1.0;
-			}else if(vel<-1){
-				accel = -1.0;
-			}
-			accelerations[i] = accel;
-		}
-		
-		// KAN IKKE BRUKE STARTPOINT fordi velocity = 0
+		// Skip start point as velocity and acceleration is 0
 		if(pointType.equals("StartPoint")){
 			splineSegments.clear();
+			System.out.println("Startpoint received");
+			setisPathFinished(false);
 		}else{
+			// Read message
+			for(int i = 0; i < jointCount ; ++i){
+				poses[i] = Double.parseDouble(lineSplt[2].split(" ")[i]);
+				if(pointType.equals("WayPoint")){
+					velocities[i]= scaleValueToOne(Math.abs(Double.parseDouble(lineSplt[3].split(" ")[i])));
+					double accel = scaleValueToOne(Math.abs(Double.parseDouble(lineSplt[4].split(" ")[i])));
+					if(accel==0.0){accel = zero;}
+					accelerations[i] = accel;
+				}
+			}
+			//String posstring = poses[0] + " " + poses[1] + " " + poses[2] + " " + poses[3] + " " + poses[4] + " " + poses[5] + " " + poses[6];
+			String velstring = velocities[0] + " " + velocities[1] + " " + velocities[2] + " " + velocities[3] + " " + velocities[4] + " " + velocities[5] + " " + velocities[6];
+			String accelstring = accelerations[0] + " " + accelerations[1] + " " + accelerations[2] + " " + accelerations[3] + " " + accelerations[4] + " " + accelerations[5] + " " + accelerations[6];
+			System.out.println("Pointtype: " + pointType + "  " + " Vel: " + velstring + " Accel: " + accelstring);
 			PTPpoint ptp = new PTPpoint(pointType, new JointPosition(poses), velocities, accelerations);
 			splineSegments.add(ptp.getPTP());
+			
 			if(pointType.equals("EndPoint")){
-				followPathThread.run();
+				System.out.println("endpoint received");
+				followPath();
 			}
 		}
 	}
+	private double scaleValueToOne(double value){
+		if(value>1){
+			value=1.0;
+			}
+		return value;
+	}
+
+	// TODO: what happens if an emergency stop is triggered?
+	private void followPath(){
+		System.out.println("empt: " + !splineSegments.isEmpty());
+		System.out.println("run: " + isNodeRunning());
+		System.out.println("stop: " + !(getEmergencyStop()));
+		if( !(getEmergencyStop()) && !splineSegments.isEmpty() && isNodeRunning()){
+			System.out.println("followpath:");
+			SplineJP spline = new SplineJP(splineSegments.toArray(new SplineMotionJP<?>[splineSegments.size()]));
+			currentmotion = lbr.moveAsync(spline, new SplineMotionListener());
+			}
+	}
 	
 	private void JointLBRMotion(String commandstr) {
-		if(isSocketConnected() && !(closed) && !(getEmergencyStop())){
+		if(isNodeRunning() && !getEmergencyStop()){
 			String []lineSplt = commandstr.split(" ");
 			int jointIndex = Character.getNumericValue(lineSplt[1].charAt(1)) - 1 ;
 			double direction = Double.parseDouble(lineSplt[2]);
 			double jointAngle;
 				if(jointIndex==-1){ // A10: Stop all
-					if(LBR_is_Moving){
+					if(getisLBRMoving()){
 						currentmotion.cancel();
 					}
-					LBR_is_Moving = false;
+					setisLBRMoving(false);
 					CommandedjointPos.set(lbr.getCurrentJointPosition());
 				}else if(jointIndex == 8){
 				 // A9: Move to driveposition
-					if(LBR_is_Moving){
+					if(getisLBRMoving()){
 						currentmotion.cancel();
 					}
 				    lbr.move(ptp(drivePos).setJointVelocityRel(defaultVelocity));
-				    LBR_is_Moving = false;
+					setisLBRMoving(false);
 				    CommandedjointPos.set(lbr.getCurrentJointPosition());
 				}else{
 					jointAngle = setJointAngle(jointIndex, direction);
 					if(!(CommandedjointPos.get(jointIndex)==jointAngle)){
 						CommandedjointPos.set(jointIndex, jointAngle);
-						if(LBR_is_Moving){
+						if(getisLBRMoving()){
 							currentmotion.cancel();
 						}
 						currentmotion = lbr.moveAsync(ptp(CommandedjointPos).setJointVelocityRel(defaultVelocity));
-						LBR_is_Moving = true;
+						setisLBRMoving(true);
 					}
 				}	
 		}
 	}
-	
 	private double setJointAngle(int jointIndex, double direction) {
 		double jointAngle = lbr.getCurrentJointPosition().get(jointIndex);
 		if(direction==-1){
@@ -233,48 +214,31 @@ public class LBR_commander extends Thread{
 	
 	public class MonitorEmergencyStopThread extends Thread {
 		public void run(){
-			while((isSocketConnected()) && (!(closed))) {
+			while(isNodeRunning()) {
 				if (getEmergencyStop()){
-						if (LBR_is_Moving && !currentmotion.equals(null)){
-							currentmotion.cancel();
+						if (getisLBRMoving()){
+							System.out.println("Lbr emergencythread");
+							if(!(currentmotion==null)){
+								currentmotion.cancel();
+							}
 							CommandedjointPos.set(lbr.getCurrentJointPosition());
 						}
-						LBR_is_Moving = false;
+						setisLBRMoving(false);
 						}
 				}
 			}
 		}
 	
-	public void setEmergencyStop(boolean es){
-		this.EmergencyStop = es;
-	}
-	
-	public boolean getEmergencyStop(){
-		return EmergencyStop;
-	}
-	
-	public void createSocket(){
-		if (this.ConnectionType == "TCP") {
-			 socket = new TCPSocket(this.port);
-		}
-		else {
-			socket = new UDPSocket(this.port);
-		}
-	}
-	
-	public boolean isSocketConnected() {
-		return socket.isConnected();
-	}
-	
 	public class MonitorLBRCommandConnectionsThread extends Thread {
 		int timeout = 3000;
 		public void run(){
 			while(!(isSocketConnected()) && (!(closed))) {
-				if(isKMPConnected) {
-					timeout = 5000;
+				if(getisKMPConnected()) {
+					timeout = connection_timeout;
 				}
 				createSocket();
 				if (isSocketConnected()){
+					setisLBRConnected(true);
 					break;
 				}
 				try {
@@ -284,30 +248,45 @@ public class LBR_commander extends Thread{
 				}
 			}
 			if(!closed){
-				System.out.println("Connection with KMP Command Node OK!");
+				System.out.println("Connection with LBR Command Node OK!");
 				runmainthread();
 				}	
 		}
 	}
 	
-	public boolean isLBRMoving() {
-		return LBR_is_Moving;
+	public class SplineMotionListener implements IMotionContainerListener {
+		public SplineMotionListener() { 
+		  }
+		@Override
+		public void onStateChanged(IExecutionContainer arg0, ExecutionState arg1) {
+			// TODO Auto-generated method stub	
+		}
+
+		@Override
+		public void containerFinished(IMotionContainer arg0) {
+			System.out.println("Container finished!");
+			setisLBRMoving(false);
+			setisPathFinished(true);
+			splineSegments.clear();
+		}
+
+		@Override
+		public void motionFinished(IMotion arg0) {
+		}
+
+		@Override
+		public void motionStarted(IMotion arg0) {
+			setisLBRMoving(true);
+		}
 	}
 	
-	public void setKMPConnected(boolean KMPConnected) {
-		this.isKMPConnected = KMPConnected;
-	}
-	
-	public boolean getShutdown() {
-		return this.shutdown;
-	}
-	
+	@Override
 	public void close(){
-		shutdown = true;
+		setShutdown(true);
 		closed = true;
 		CommandedjointPos.set(lbr.getCurrentJointPosition());
 
-		if(LBR_is_Moving){
+		if(getisLBRMoving()){
 			try{
 			currentmotion.cancel();
 			}catch(Exception e){
@@ -318,6 +297,6 @@ public class LBR_commander extends Thread{
 		System.out.println("LBR command closed!");
 
 	}
-	
+
 	
 }
