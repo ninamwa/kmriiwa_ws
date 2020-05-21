04fce89271014cd2082205dae0b8fa84b5b16eaa
ninamwa <ninamwa@stud.ntnu.no>
2020-05-21 10:18:01 +0200

    new parameters

--- kmr_slam/launch/rtabmap.launch.py
+++ kmr_slam/launch/rtabmap.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
     rtabmap_parameters=[{
         'frame_id':'base_footprint',
         'use_sim_time':use_sim_time,
-        'subscribe_rgbd':True,
+        'subscribe_rgbd':False,
         'subscribe_rgb':False,
         'subscribe_scan':False,
         'subscribe_depth':False,
@@ -20,12 +20,29 @@ def generate_launch_description():
         'approx_sync':True,
         'queue_size':20,
         'rgbd_cameras':1,
-        'Grid/FromDepth':'False',
+
+        #'Grid/FromDepth':'False',
         'Grid/RayTracing':'True',
         'Grid/3D':'True',
+
         'Grid/NoiseFilteringRadius':'0',
+        'Grid/NoiseFilteringMinNeighbors':'6',
+
+        'Grid/RangeMax':'3.5',
+        'Vis/MaxDepth':'8.0',
+        'Grid/MaxObstacleHeight':'2.0',
+
+
         'Grid/NormalSegmentation':'False',
-        #'map_always_update':True
+        
+        'Reg/Strategy':'0',
+        'Reg/Force3DoF':'True',
+        'Vis/EstimationType':'0',
+        'Vis/MinInliers':'10',
+        'Vis/InlierDistance':'0.02',
+        'Vis/CorGuessWinSize':'0',
+        'OdomF2M/BundleAdjustment':'0',
+        'Optimizer/Slam2D':'True',
 
     }]
 
@@ -40,6 +57,13 @@ def generate_launch_description():
         ('rgb/camera_info', '/camera2/camera/color/camera_info'),
         ('rgbd_image','/rgbd_image1'),
         ('depth/image', '/camera2/camera/depth/image_rect_raw')
+    ]
+
+    remappings3=[
+        ('rgb/image', '/camera3/camera/color/image_raw'),
+        ('rgb/camera_info', '/camera3/camera/color/camera_info'),
+        ('rgbd_image','/rgbd_image2'),
+        ('depth/image', '/camera3/camera/depth/image_rect_raw')
 
     ]
 
@@ -52,11 +76,7 @@ def generate_launch_description():
 	    ('scan','/scan_1')]
 
     remappings_scancloud=[
-        ('scan_cloud','/combined_cloud'),
-        ('rgb/image','mycamera/image_demo'),
-        ('rgb/camera_info', 'mycamera/image_demo/camera_info'),
-        ('depth/image', 'mycamera/depth_demo')
-    ]
+        ('scan_cloud','/combined_cloud')]
 
           
 
@@ -73,13 +93,18 @@ def generate_launch_description():
         Node(
             package='rtabmap_ros', node_name='sync1', node_executable='rgbd_sync', output='screen',
             parameters=[{'approx_sync':True, 'use_sim_time':use_sim_time}],
-            remappings=remappings_gazebo,
+            remappings=remappings1,
             emulate_tty=True),
-        #Node(
-        #    package='rtabmap_ros', node_name='sync2', node_executable='rgbd_sync', output='screen',
-        #    parameters=[{'approx_sync':True, 'use_sim_time':use_sim_time}],
-        #    remappings=remappings2,
-        #    emulate_tty=True),
+        Node(
+           package='rtabmap_ros', node_name='sync2', node_executable='rgbd_sync', output='screen',
+           parameters=[{'approx_sync':True, 'use_sim_time':use_sim_time}],
+           remappings=remappings2,
+           emulate_tty=True),
+        Node(
+           package='rtabmap_ros', node_name='sync3', node_executable='rgbd_sync', output='screen',
+           parameters=[{'approx_sync':True, 'use_sim_time':use_sim_time}],
+           remappings=remappings3,
+           emulate_tty=True),
 
         Node(
             package='rtabmap_ros', node_executable='rtabmap', output='screen',
@@ -94,26 +119,28 @@ def generate_launch_description():
 	        remappings=remappings_scancloud,
             emulate_tty=True),
 
-        Node(
-           package='pointcloud_to_laserscan',
-           node_executable='laserscan_to_pointcloud_node',
-           node_name='laserscan_to_pointcloud1',
-           output='screen',
-           parameters=[{'use_sim_time': use_sim_time}],
-           remappings=[('scan_in', 'scan'),
-                       ('cloud', 'cloud1')],
-           ),
-        Node(
-           package='pointcloud_to_laserscan',
-           node_executable='laserscan_to_pointcloud_node',
-           node_name='laserscan_to_pointcloud2',
-           output='screen',
-           parameters=[{'use_sim_time': use_sim_time}],
-           remappings=[('scan_in', 'scan_2'),
-                       ('cloud', 'cloud2')],
-           ),
+        # Node(
+        #    package='pointcloud_to_laserscan',
+        #    node_executable='laserscan_to_pointcloud_node',
+        #    node_name='laserscan_to_pointcloud1',
+        #    output='screen',
+        #    parameters=[{'use_sim_time': use_sim_time}],
+        #    remappings=[('scan_in', 'scan_1'),
+        #                ('cloud', 'cloud1')],
+        #    ),
+        # Node(
+        #    package='pointcloud_to_laserscan',
+        #    node_executable='laserscan_to_pointcloud_node',
+        #    node_name='laserscan_to_pointcloud2',
+        #    output='screen',
+        #    parameters=[{'use_sim_time': use_sim_time}],
+        #    remappings=[('scan_in', 'scan_2'),
+        #                ('cloud', 'cloud2')],
+        #    ),
 
         Node(
             package='rtabmap_ros', node_executable='point_cloud_aggregator', output='screen',
+            remappings=[('cloud1','points2_1'),('cloud2','points2_2'),('cloud3','points2_3')],
+            parameters=[{'count': 3}],
             emulate_tty=True,),
     ])
